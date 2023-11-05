741cdf862e359fd5cdc7d83eae74abf4e3e98bf5
gnoparus <surapong@gmail.com>
2023-11-05 17:24:50 +0700

    Add logging in onSignup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,7 +13,7 @@ export default function SignupPage() {
     })
 
     const onSignup = async () => {
-
+        console.log("onSignup")
     }
 
     return (<div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -25,7 +25,7 @@ export default function SignupPage() {
         <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="email" type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="email" />
         <label htmlFor="email">password</label>
         <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="password" type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="password" />
-        <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="button">Signup here</button>
+        <button onClick={onSignup} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="button">Signup here</button>
         <Link href="/login">Visit login</Link>
     </div>)
 }
