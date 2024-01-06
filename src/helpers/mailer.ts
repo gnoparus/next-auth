e19a79125510e4eb3b4b2bf6ce4fc1f82c5ac1f2
gnoparus <surapong@gmail.com>
2024-01-06 15:01:40 +0700

    externalize from email

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
         });
 
         const mailOptions = {
-            from: 'test@example.com',
+            from: process.env.NODEMAILER_FROM_EMAIL,
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
             html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
